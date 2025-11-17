e3c0ff0761b0eeaae51f8affb205e87c8e06d838
ravi00242-design <ravi00242@gmail.com>
2025-11-17 16:46:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ import json
 # ---------------------------------------------------------
 # CONFIG — LM Studio endpoint
 # ---------------------------------------------------------
-LMSTUDIO_URL = "LMSTUDIO_URL = "https://guidelines-die-racial-gui.trycloudflare.com/v1/chat/completions"
+LMSTUDIO_URL = "https://guidelines-die-racial-gui.trycloudflare.com/v1/chat/completions"
 "  # Default LM Studio server
 MODEL_NAME = "gemma-2-2b-it"  # LM Studio automatically assigns internal model names, optional
 
